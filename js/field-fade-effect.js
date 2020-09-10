5e1c131ca660b8a4acb20da611d9e43be28834be
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-09-09 22:34:32 -0600

    add styles.css

--- js/field-fade-effect.js
+++ js/field-fade-effect.js
@@ -1,44 +1,63 @@
-function fadeOverflowText(elm) {
-  var $elm = $(elm);
-  var $parent = $elm.parent();
-  var overflowLeft = elm.scrollLeft > 0;
-  var overflowRight = elm.scrollWidth - ($elm.innerWidth() + elm.scrollLeft) > 2;//2px margin of error
-  overflowLeft ? $parent.addClass('overflow-fade-left') : $parent.removeClass('overflow-fade-left');
-  overflowRight ? $parent.addClass('overflow-fade-right') : $parent.removeClass('overflow-fade-right');
-  if ($elm.attr('data-toggle') === 'password') {
-      overflowRight ? $parent.addClass('overflow-fade-pwd-right') : $parent.removeClass('overflow-fade-pwd-right');
-  }
-}
+window.onload = (event) => {
+    let field = document.getElementById('field');
+
+    // fadeOverflowText(field);
+
+    field.addEventListener('click', () => {
+        console.log('WTF!');
+    });
+
+    ['keyup','keydown'].forEach(event => {
+        field.addEventListener(event, () => fadeOverflowText(field));
+        // field.addEventListener(event, () => console.log('WTF?'));
+        
+    });
+
+    // field.addEventListener('keyup, keydown', () => {
+    //     console.log('WTF!');
+    //     fadeOverflowText(this);
+    // });
+
+    // field.addEventListener('focusout', function() {
+    //     setTimeout(function () {
+    //         //short timeout to let text shift before calculating overflow
+    //         fadeOverflowText(this);
+    //     },
+    //     10);
+    // })
+};
+
+
 
 function checkTextOverflow() {
   var inputEntry = $('input[type="text"]:not(.mask), input[type="password"]');
   
-  //bind document selectionchange to custom selectionEnd function to apply fade overlay on iPhone/iPad selection change
-  document.onselectionchange = userSelectionChanged;
-  var selectionEndTimeout;
+//   //bind document selectionchange to custom selectionEnd function to apply fade overlay on iPhone/iPad selection change
+//   document.onselectionchange = userSelectionChanged;
+//   var selectionEndTimeout;
    
-  $(window).bind('selectionEnd', function () {
+//   $(window).bind('selectionEnd', function () {
   
-      // reset selection timeout
-      selectionEndTimeout = null;
+//       // reset selection timeout
+//       selectionEndTimeout = null;
   
-      // TODO: Do your cool stuff here........
-      inputEntry.each(function() {
-          fadeOverflowText(this);
-      })
-  });
+//       // TODO: Do your cool stuff here........
+//       inputEntry.each(function() {
+//           fadeOverflowText(this);
+//       })
+//   });
   
-  function userSelectionChanged() {
+//   function userSelectionChanged() {
   
-      // wait 500 ms after the last selection change event
-      if (selectionEndTimeout) {
-          clearTimeout(selectionEndTimeout);
-      }
+//       // wait 500 ms after the last selection change event
+//       if (selectionEndTimeout) {
+//           clearTimeout(selectionEndTimeout);
+//       }
   
-      selectionEndTimeout = setTimeout(function () {
-          $(window).trigger('selectionEnd');
-      }, 50);
-  }
+//       selectionEndTimeout = setTimeout(function () {
+//           $(window).trigger('selectionEnd');
+//       }, 50);
+//   }
   
   //first page load
   inputEntry.each(function (event, element) {
@@ -52,15 +71,23 @@ function checkTextOverflow() {
   
   //on focusout
   inputEntry.on('focusout change', function (event, element) {
-      // Cache references
-      var $el = $(this);
-      //reset before calculating - necessary?
-      $el.parent().removeClass('overflow-fade-right').removeClass('overflow-fade-left');
-
       setTimeout(function () {
           //short timeout to let text shift before calculating overflow
-          fadeOverflowText($el[0]);
+          fadeOverflowText(this);
       },
       10);
   });
 }
+
+function fadeOverflowText(elm) {
+    // var $elm = $(elm);
+    var parent = elm.parentElement;
+    var overflowLeft = elm.scrollLeft > 0;
+    // var overflowRight = elm.scrollWidth - ($elm.innerWidth() + elm.scrollLeft) > 2;//2px margin of error
+    var overflowRight = elm.scrollWidth - (elm.clientWidth + elm.scrollLeft) > 2;//2px margin of error
+    overflowLeft ? parent.classList.add('overflow-fade-left') : parent.classList.remove('overflow-fade-left');
+    overflowRight ? parent.classList.add('overflow-fade-right') : parent.classList.remove('overflow-fade-right');
+    // if ($elm.attr('data-toggle') === 'password') {
+    //     overflowRight ? $parent.addClass('overflow-fade-pwd-right') : $parent.removeClass('overflow-fade-pwd-right');
+    // }
+  }
