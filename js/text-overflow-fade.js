ad110c854c77df914695c6310c6c1d1db66751a2
alexjmarshall <43717456+alexjmarshall@users.noreply.github.com>
2020-09-11 14:44:26 -0600

    cleanup text-overflow-fade.js

--- js/text-overflow-fade.js
+++ js/text-overflow-fade.js
@@ -1,36 +1,36 @@
 window.onload = () => {
-    let fields = document.querySelectorAll('.fade-field');
+  let fields = document.querySelectorAll('.fade-field');
 
-    fields.forEach(field => {
-        //apply overflow fade on load
-        fadeOverflowText(field);
+  fields.forEach(field => {
+    //apply overflow fade on load
+    fadeOverflowText(field);
 
-        ['keyup','keydown','touchend'].forEach(event => {
-            field.addEventListener(event, () => fadeOverflowText(field));
-        });
-
-        field.addEventListener('blur', () => {
-            //timeout to let text shift before calculating text overflow
-            setTimeout(() => {
-                fadeOverflowText(field);
-            },10);
-        });
+    ['keyup','keydown','touchend'].forEach(event => {
+      field.addEventListener(event, () => fadeOverflowText(field));
     });
 
-    //apply overflow fade on selection change
-    document.onselectionchange = () => {
-        let activeElm = document.activeElement;
-        let fieldsArr = Array.from(fields);
-        if (fieldsArr.includes(activeElm))
-            fadeOverflowText(activeElm);
-    };
+    field.addEventListener('blur', () => {
+      //timeout to let text shift before calculating text overflow
+      setTimeout(() => {
+        fadeOverflowText(field);
+      },10);
+    });
+  });
+
+  //apply overflow fade on selection change
+  document.onselectionchange = () => {
+    let activeElm = document.activeElement;
+    let fieldsArr = Array.from(fields);
+    if (fieldsArr.includes(activeElm))
+      fadeOverflowText(activeElm);
+  };
 };
 
 function fadeOverflowText(elm) {
-    let parent = elm.parentElement;
-    let overflowLeft = elm.scrollLeft > 0;
-    let overflowRight = elm.scrollWidth - (elm.clientWidth + elm.scrollLeft) > 2;//2px margin of error
+  let parent = elm.parentElement;
+  let overflowLeft = elm.scrollLeft > 0;
+  let overflowRight = elm.scrollWidth - (elm.clientWidth + elm.scrollLeft) > 2;//2px margin of error
     
-    overflowLeft ? parent.classList.add('overflow-fade-left') : parent.classList.remove('overflow-fade-left');
-    overflowRight ? parent.classList.add('overflow-fade-right') : parent.classList.remove('overflow-fade-right');
+  overflowLeft ? parent.classList.add('overflow-fade-left') : parent.classList.remove('overflow-fade-left');
+  overflowRight ? parent.classList.add('overflow-fade-right') : parent.classList.remove('overflow-fade-right');
 }
